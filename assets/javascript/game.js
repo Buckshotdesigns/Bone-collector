fafd8c72b6588688019f129c14edd5a896d19636
bryanbuckus <bryanbuckus@gmail.com>
2019-02-18 09:33:25 -0500

    pseudo code and game descroption

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,8 @@
 
 
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
 
 
 
@@ -10,3 +13,8 @@ $(document).ready(function() {
     
 });
 
+// assign random number to guess 
+// start with a zero wins and losses that increment after each game
+// assign random variable to each of the 4 skeletons
+// set the total score to zero 
+// add to the score each time a skeleton is clicked
