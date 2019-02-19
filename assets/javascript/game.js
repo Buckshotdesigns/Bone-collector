4044486b84452824e8731742105a827e470dc434
bryanbuckus <bryanbuckus@gmail.com>
2019-02-19 15:25:14 -0500

    added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,41 @@
 
+    
+
+$(document).ready(function() {
 
 
+    // global variables
     var wins = 0;
     var losses = 0;
     var playerScore = 0;
     var boneOptions = [];
     
-
-$(document).ready(function() {
-
-    
-
-    
+    // defining as global variable to prevent scope issue
     var targetNumber = 0;
-  
+//   function to start and restart game 
     resetGame();
 
     function resetGame() {
-
+// resets scores back to 0 after game restarts
     targetNumber = 0;
     playerScore = 0;
     
     
-   
-    targetNumber =[Math.floor(Math.random() * 102 + 19)];
+// generates random target number between 1-120 for user to get
+    targetNumber =[Math.floor(Math.random() * 101 + 19)];
     console.log("second number" + targetNumber);
     // pushes random number to target score heading 
     $("#target-score").text(targetNumber);
 
 
    
-    // assigning array value to each of the skeleton images and appending them to the screen
+    // assigning 4 random numbers to use for skeleton images 
     for (var j = 0; j < 4; j++){
         
             boneOptions[j] = Math.floor(Math.random() * 12) + 1;
 
     };
-
+// assigning attribute value to each of the skeleton images and putting them in html
     for (var j = 0;j < 4;j++){
 
         $("#bone-image-"+(j+1)).attr("val", boneOptions[j]);
@@ -45,17 +44,17 @@ $(document).ready(function() {
 };
 
 
-
+// click function on each of the skeleton images 
     $(".bone-image").on("click", function() {
-
+// grabs the number in the attribute value after clicking the skeleton 
         var boneValue = ($(this).attr("val"));
         boneValue = parseInt(boneValue);
    
-
+// adds each of the numbers clicked from the skeleton to the player score 
         playerScore += boneValue;
-        console.log(playerScore);
+        
     
-       
+    //sets the win condition if you get the target number and alerts user they won before resetting the game
         if (playerScore == targetNumber) {
             alert("You Win!!!!");
             wins ++;
@@ -63,6 +62,7 @@ $(document).ready(function() {
            resetGame();
            
         }
+    // sets the losing condition and alerts the user they lost before resetting the game 
         else if (playerScore > targetNumber) {
             alert ("You Lose!!!!!");
             losses ++;
