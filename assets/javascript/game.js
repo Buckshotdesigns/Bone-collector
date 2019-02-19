5ee954b8989c707cf91ce9db4f41e14f40f318bd
bryanbuckus <bryanbuckus@gmail.com>
2019-02-19 15:12:16 -0500

    fixed a lot of stuff. reworked the attr assigning and added images directly to the html and redefined global variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,54 +4,51 @@
     var wins = 0;
     var losses = 0;
     var playerScore = 0;
+    var boneOptions = [];
     
-    
-    var image = [
-    
-    "assets/images/bones1.png",
-    "assets/images/bones2.png",
-    "assets/images/bones3.png",
-    "assets/images/bones4.png",
-    
-    ]
 
 $(document).ready(function() {
 
     
 
-// generate random number between 30 and 100 for the target score
+    
+    var targetNumber = 0;
+  
+    resetGame();
 
-var targetNumber =[Math.floor(Math.random() * 69 + 30)];
-    console.log(targetNumber);
+    function resetGame() {
+
+    targetNumber = 0;
+    playerScore = 0;
+    
+    
+   
+    targetNumber =[Math.floor(Math.random() * 102 + 19)];
+    console.log("second number" + targetNumber);
     // pushes random number to target score heading 
     $("#target-score").text(targetNumber);
 
 
    
     // assigning array value to each of the skeleton images and appending them to the screen
-    for (var j = 0; j < image.length; j++){
-
-        var boneImage = $("<img>");
-
-        boneImage.addClass("bone-image");
-
-        boneImage.attr("src",  image[j]);
-
-            // getting the random value associated to the four skeletons
+    for (var j = 0; j < 4; j++){
         
-            var boneOptions = Math.floor(Math.random() * 14 + 1);
-            
-           
-
-        boneImage.attr("data-bone-value", boneOptions);
-   
-        $("#bone-img").append(boneImage);
+            boneOptions[j] = Math.floor(Math.random() * 12) + 1;
 
     };
 
+    for (var j = 0;j < 4;j++){
+
+        $("#bone-image-"+(j+1)).attr("val", boneOptions[j]);
+        
+    };
+};
+
+
+
     $(".bone-image").on("click", function() {
 
-        var boneValue = ($(this).attr("data-bone-value"));
+        var boneValue = ($(this).attr("val"));
         boneValue = parseInt(boneValue);
    
 
@@ -62,12 +59,16 @@ var targetNumber =[Math.floor(Math.random() * 69 + 30)];
         if (playerScore == targetNumber) {
             alert("You Win!!!!");
             wins ++;
+            
            resetGame();
+           
         }
-        else if (playerScore >= targetNumber) {
+        else if (playerScore > targetNumber) {
             alert ("You Lose!!!!!");
             losses ++;
+            
             resetGame();
+           
         }
     // pushes player score to the page
     $("#player-score").text(playerScore);
@@ -77,28 +78,6 @@ var targetNumber =[Math.floor(Math.random() * 69 + 30)];
     $("#losses").text( losses);
 
     });
-
-    function resetGame() {
-        
-    playerScore = 0;
-
-    targetNumber =[Math.floor(Math.random() * 69 + 30)];
-    console.log(targetNumber);
-    // pushes random number to target score heading 
-    $("#target-score").text(targetNumber);
-
-    for (var j = 0; j < 4; j++){
-
-       
-
-        var boneOptions = Math.floor(Math.random() * 14 + 1);
-
-        boneImage.attr("data-bone-value", boneOptions);
-
-
-    };
-
-    };
 
 
 });
